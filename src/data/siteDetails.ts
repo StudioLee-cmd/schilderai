638c7aee19936ce711cf023e7c772cf61301fcc7
StudioLee-cmd <tim@studiolee.nl>
2026-03-05 01:02:51 +0100

    Fix siteDetails: niche field, live siteUrl, siteName, language nl-NL; FreeTrialModal uses niche

--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,11 +1,12 @@
 export const siteDetails = {
     siteName: 'SchilderAI',
-    siteUrl: 'https://schilderai.nl/',
+    niche: 'Schilders',
+    siteUrl: 'https://www.schilderai.nl/',
     metadata: {
         title: 'SchilderAI - De slimme assistent voor jouw schildersbedrijf',
         description: 'Bespaar zeeën van tijd en zeg vaarwel tegen saai kantoorwerk. SchilderAI regelt je agenda, telefoon en administratie volledig automatisch.',
     },
-    language: 'nl-nl',
+    language: 'nl-NL',
     locale: 'nl-NL',
     siteLogo: '/images/logo-transparent.png', // Updated to new provided logo
     googleAnalyticsId: '', // e.g. G-XXXXXXX,
